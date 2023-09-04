199c11f2bc6ea59655582b7a39c331a116a43cb6
Deljeet Kumar <uic.21mca2073@gmail.com>
2023-09-04 14:48:15 +0530

    new changes

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-
+import './index.css'
 const Counter = () =>{
     const [count, setCount] = useState(0);
     
@@ -12,11 +12,14 @@ const Counter = () =>{
 
 
     return(
+
         <>
+        <div className='container'>
         <h1>Counter App</h1>
         <p>Count: {count}</p>
         <button onClick={increment}>Increment</button>
         <button onClick={decrement}>IDecrement</button>
+        </div>
         </>
     )
 }
